beb1538382f8909f39922a9f97e63e82b10ad1a6
d4rkFox <maks.petri4enko@yandex.ru>
2020-05-21 12:26:52 +0700

    added product-sluder and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 3.6,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    var mixer = mixitup(".products__inner-box");
+
+$(".product-slider__inner").slick({
+    dots:true,
+    arrows:false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+
+})
+
 });
