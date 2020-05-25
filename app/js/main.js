cb049d96da5f32c0b34a271c48203c86aed22bec
d4rkFox <maks.petri4enko@yandex.ru>
2020-05-25 21:36:54 +0700

    new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,39 @@
-$(function(){
+$(function () {
 
     $(".rate-star").rateYo({
         rating: 3.6,
         starWidth: "12px",
         readOnly: true
-      });
+    });
+
+    $(".product-slider__inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    })
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function () {
+        $('.product-page__contant .products__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active')
+    });
+
+    $('.icon-th-large').on('click', function () {
+        $('.product-page__contant .products__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
 
     var mixer = mixitup(".products__inner-box");
-
-$(".product-slider__inner").slick({
-    dots:true,
-    arrows:false,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-
-})
 
 });
